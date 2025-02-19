49806cf53d32e1511f525d185392d80d6207b4fc
mabdullahjs <mabdullah2037@outlook.com>
2025-02-19 15:37:59 +0500

    introduction

---
+++ 02-Javascript/01-Introduction/app.js
@@ -0,0 +1,48 @@
+// console.log("Hello world!");
+// console.log("hello abdullah");
+
+
+
+// Data types
+
+
+// string "asdasds" '20' 'dasdas'
+// number 20.2321
+// boolean true | false
+
+// object
+// bigint
+// symbol
+
+
+
+// console.log('hello world');
+// console.log(2025);
+// console.log(true);
+
+// firstName
+// 12username xx
+// username123
+// userna()me* xx
+// $_username
+
+
+// var username = "abdullah";
+// var email = "mabdullah2037@gmail.com";
+// var age = 21
+// var isLoggedIn = true
+
+// // console.log(username);
+// // console.log(email);
+// console.log(typeof (isLoggedIn));
+// console.log(isLoggedIn);
+
+
+
+// var city = "karachi"
+// console.log(city);
+
+
+
+// var age = "21";
+// console.log(typeof (age))
