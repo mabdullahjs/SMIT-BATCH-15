44518811234415c959ee2bdc9a7e34fb3d9dfc09
mabdullahjs <mabdullah2037@outlook.com>
2025-02-21 15:55:25 +0500

    variable

---
+++ 02-Javascript/02-Variables and Data Types/app.js
@@ -0,0 +1,143 @@
+// string
+// number
+// boolean
+
+// object
+// bigint
+// symbol
+// null
+// undefined
+
+
+
+
+
+
+// console.log(typeof('sdasdasd'))
+// console.log(typeof(12.21))
+// console.log(typeof(true))
+
+
+
+
+
+
+
+
+
+// var username = "abdullah";
+// console.log('username')
+
+
+
+
+// alert('hello world')
+// console.log("console after alert");
+
+
+// var username = prompt('enter your name');
+// console.log(username);
+
+
+
+
+// username
+// age
+// city
+// email
+
+
+// var username = "Abdullah";
+// console.log(username);
+
+// var username = "Usman";
+// console.log(username);
+
+
+
+// alert('hello')
+// var username = prompt("enter your username")
+// console.log('hello world');
+
+
+
+
+// var username = prompt("enter your username");
+// var age = prompt("enter your age");
+// var email = prompt("enter your email");
+
+// console.log(username)
+// console.log(email)
+// console.log(age)
+
+
+
+
+
+
+// var num1 = 20;
+// var num2 = 40;
+// var num3 = '60';
+
+// console.log(num1 + num2 + num3)
+
+// concatination
+
+// var username = "abdullah";
+// var email = "mabdullah@gmail.com"
+
+// console.log(username + email);
+
+
+
+
+// var username = "abdullah"; // declare krna // initialize krna
+
+
+// var username; // declare krna
+// username = "Abdullah" // assign krdi
+// console.log(username);
+
+
+// var username = "abdullah";
+// username = "usman" // reassign
+
+// console.log(username);
+
+
+// var username = "abdullah";
+// var username = "usman" // redeclare
+
+// console.log(username);
+
+
+
+
+
+
+
+
+
+// var num1 = prompt('enter num1')
+// var num2 = prompt('enter num2')
+
+
+// console.log(num1 + num2)
+
+
+
+
+
+// percentage calculator
+
+
+
+
+
+
+
+
+
+
+
+
