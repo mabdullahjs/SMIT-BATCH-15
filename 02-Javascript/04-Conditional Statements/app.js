43249cf28a18d67f86d961c9cbd3777a72ada333
mabdullahjs <mabdullah2037@outlook.com>
2025-03-03 13:59:16 +0500

    conditional statement

---
+++ 02-Javascript/04-Conditional Statements/app.js
@@ -0,0 +1,142 @@
+// console.log('hello world');
+
+
+// if (10 > 5){
+//     console.log('10 is greater than 5')
+// } else {
+//     console.log('10 is less than 5')
+// }
+
+
+
+
+
+
+
+
+// var age = 12;
+
+// if(age >= 16){
+//     console.log('admission miljayega');
+// }else {
+//     console.log('admission nahi milaiga');
+// }
+
+
+
+
+
+
+
+
+
+
+// var num = +prompt('enter number');
+
+// if(num === 50){
+//     console.log('number is equals to 50');
+// } else {
+//     console.log('number is not equals to 50');
+    
+// }
+
+
+// 18 sa bara ha to admission miljayega
+// 16 sa bara or 18 sa chota ha to 500 zyada daika admission milaiga.
+// warna admission nahi milaiga
+
+
+// var fullName = 'Muhammad Abdullah';
+// var age = 10;
+
+// if(age >= 18){
+//     console.log(fullName + " welcome to gym");
+    
+// } else if (age >= 16){
+//     console.log(fullName + " maalik 500 zyaa doo or admission miljayega");
+    
+// } else if (age >= 14){
+//     console.log(fullName + " maalik 1500 zyaa doo or admission miljayega");
+    
+// } else {
+//     console.log(fullName + " maalik kaam kro apna");
+    
+// }
+
+
+
+
+
+
+
+
+
+
+// bankbalance > 1500000 == shaadi mubarak
+// bankbalance > 800000 == Istekhara krwata hain
+// bankbalance > 500000 == Istekhara ma naa agaya ha
+// bankbalance > 200000 == Abbu nahi maan rha
+// bankbalance > 50000 == Ammi chat sa chalang marlaingi.
+
+
+
+
+
+
+
+
+
+// number is divisible by 3
+
+// var num = 540;
+
+// if(num % 3 === 0){
+//     console.log('number is divisible by 3');
+    
+// } else {
+//     console.log('number is not divisible by 3');
+
+// }
+
+
+
+
+var num1 = +prompt('enter num1')
+var num2 = +prompt('enter num2')
+var operator = prompt('enter + - * / %');
+
+if(operator === "+"){
+    console.log("addition" ,num1 + num2);
+} else if(operator === '-'){
+    console.log("subtraction" , num1 - num2);
+    
+} else {
+    console.log('invalid operator');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
