c9b55e3ef1d45213db33691b3c94e2b136faa3bf
mabdullahjs <mabdullah2037@outlook.com>
2025-03-12 13:36:58 +0500

    dom manipulation

--- 02-Javascript/06-Dom-manipulation/app.js
+++ 02-Javascript/06-Dom-manipulation/app.js
@@ -10,19 +10,62 @@
 // var para = document.querySelector('#para')
 // para.innerHTML = "change krdia ha bhai javascript ko use krta hua"
 
-var h1 = document.querySelector('#head');
+// var h1 = document.querySelector('#head');
 
 
-function greetUser(){
+// function greetUser(){
 
-    // console.log(h1.innerHTML === "Hello DOM");
-    
-    
-    if(h1.innerHTML === "Hello DOM"){
-        h1.innerHTML = "changed through js"
-    }else {
-        h1.innerHTML = "Hello DOM"
-    }
-    
+//     // console.log(h1.innerHTML === "Hello DOM");
+
+
+//     if(h1.innerHTML === "Hello DOM"){
+//         h1.innerHTML = "changed through js"
+//     }else {
+//         h1.innerHTML = "Hello DOM"
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector('#head');
+// console.log(head.innerHTML);
+
+// head.innerHTML = "change through javascript"
+// head.style.backgroundColor = "red"
+// head.style.color = "white"
+
+
+
+
+
+
+
+// var email = document.querySelector('#email');
+// var password = document.querySelector('#password');
+
+// function login() {
+//     console.log(email.value);
+//     console.log(password.value);
+
+// }
+
+
+var input = document.querySelector('#username');
+var h1 = document.querySelector('#greetings');
+
+function greetUser() {
+    h1.innerHTML = "hello " + input.value
 }
-
