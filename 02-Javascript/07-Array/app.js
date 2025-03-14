33b73897dc7b8db4c748dbe8eacd83b89c7f1448
mabdullahjs <mabdullah2037@outlook.com>
2025-03-14 15:23:39 +0500

    array

---
+++ 02-Javascript/07-Array/app.js
@@ -0,0 +1,106 @@
+// var username = document.querySelector('#username');
+// var rollNumber = document.querySelector('#rollNumber');
+// var chemMarks = document.querySelector('#chemMarks');
+// var phyMarks = document.querySelector('#phyMarks');
+// var compMarks = document.querySelector('#compMarks');
+// var mathsMarks = document.querySelector('#mathsMarks');
+// var engMarks = document.querySelector('#engMarks');
+// var stuName = document.querySelector('#stuName');
+// var stuRollNum = document.querySelector('#stuRollNum');
+// var stuPercentage = document.querySelector('#stuPercentage');
+// var stuGrade = document.querySelector('#stuGrade');
+
+
+
+// function calculateResult() {
+//     var totalMarks = 500;
+//     var grade;
+//     var obtainedMarks = +chemMarks.value + +phyMarks.value + +compMarks.value + +engMarks.value + +mathsMarks.value
+
+//     var percentage = obtainedMarks / totalMarks * 100
+
+
+//     if (percentage >= 80) {
+//         grade = "A+"
+//     } else if (percentage >= 70) {
+//         grade = "A"
+//     } else if (percentage >= 60) {
+//         grade = "B"
+//     } else if (percentage >= 50) {
+//         grade = "C"
+//     } else {
+//         grade = "Fail"
+//     }
+
+//     stuName.innerHTML = "Username: " + username.value
+//     stuRollNum.innerHTML = "Roll Number: " + rollNumber.value
+//     stuPercentage.innerHTML = "Percentage: " + percentage + " %"
+//     stuGrade.innerHTML = "Grade: " + grade
+
+// }
+
+
+
+// // 80 >= A+
+// // 70 >= A
+// // 60 >= B
+// // 50 >= C
+
+
+
+
+
+
+
+
+
+
+// var fruits = ['watermelon', 'melon', 'banana']
+// console.log(fruits[3]);
+
+
+// length
+// index
+
+
+
+
+
+
+// var passedStudents = ['abdullah', 'usman', 'ammar', 'ali', 'rafay']
+// console.log(passedStudents[3]);
+
+
+
+// array methods
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+var fruits = ['watermelon', 'banana', 'apple', 'grapes', 'orange', 'mango', 'strawberry', 'pineapple', 'kiwi', 'cherry']
+
+// fruits.push('abc', 'def')r
+// fruits.splice(5, 3)
+// fruits.splice(8, 2)
+// fruits.splice(3, 1)
+// fruits.splice(6, 0, 'newVal', 'abc', 'wderf')
+// console.log(fruits.length);
+// fruits.push('lychee')
+// fruits.unshift("abcd")
+// fruits.pop()
+// fruits.shift()
+
+
+console.log(fruits);
+
